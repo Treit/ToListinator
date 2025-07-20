9ee46c72a8f244cf1a0879314f3fc25aea3d5d1a
Mike Treit <mtreit@ntdev.microsoft.com>
2025-07-20 15:38:06 -0700

    More pattern matching

--- src/ToListinator/ToListForEachAnalyzer.cs
+++ src/ToListinator/ToListForEachAnalyzer.cs
@@ -2,7 +2,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System;
 using System.Collections.Immutable;
 
 namespace ToListinator
@@ -20,7 +19,7 @@ namespace ToListinator
             isEnabledByDefault: true);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-            => ImmutableArray.Create(Rule);
+            => [Rule];
 
         public override void Initialize(AnalysisContext context)
         {
@@ -33,31 +32,37 @@ namespace ToListinator
         {
             var invocation = (InvocationExpressionSyntax)context.Node;
 
-            if (invocation.Expression is MemberAccessExpressionSyntax forEachAccess &&
-                forEachAccess.Name.Identifier.Text == "ForEach")
-            {
-                if (forEachAccess.Expression is InvocationExpressionSyntax toListInvocation &&
-                    toListInvocation.Expression is MemberAccessExpressionSyntax toListAccess &&
-                    toListAccess.Name.Identifier.Text == "ToList")
+            // Check if this matches the pattern someExpression.ToList().ForEach(...)
+            if (invocation.Expression is not MemberAccessExpressionSyntax
                 {
-                    var toListSymbol = context.SemanticModel.GetSymbolInfo(toListInvocation).Symbol as IMethodSymbol;
-                    var forEachSymbol = context.SemanticModel.GetSymbolInfo(invocation).Symbol as IMethodSymbol;
-
-                    if (toListSymbol?.Name == "ToList" &&
-                        toListSymbol.ReturnType is INamedTypeSymbol toListReturnType &&
-                        toListReturnType.OriginalDefinition.ToDisplayString() == "System.Collections.Generic.List<T>" &&
-                        forEachSymbol?.Name == "ForEach" &&
-                        SymbolEqualityComparer.Default.Equals(forEachSymbol.ContainingType, toListReturnType))
+                    Name.Identifier.Text: "ForEach",
+                    Expression: InvocationExpressionSyntax
                     {
-                        var diagnostic = Diagnostic.Create(
-                            Rule,
-                            invocation.GetLocation()
-                        );
-
-                        context.ReportDiagnostic(diagnostic);
-                    }
-                }
+                        Expression: MemberAccessExpressionSyntax { Name.Identifier.Text: "ToList" }
+                    } toListInvocation
+                })
+            {
+                return;
             }
+
+            // Verify it's actually List<T> and not something custom.
+            var toListSymbol = context.SemanticModel.GetSymbolInfo(toListInvocation).Symbol as IMethodSymbol;
+            var forEachSymbol = context.SemanticModel.GetSymbolInfo(invocation).Symbol as IMethodSymbol;
+
+            if (IsToListForEachPattern(toListSymbol, forEachSymbol))
+            {
+                var diagnostic = Diagnostic.Create(Rule, invocation.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static bool IsToListForEachPattern(IMethodSymbol? toListSymbol, IMethodSymbol? forEachSymbol)
+        {
+            return toListSymbol is { Name: "ToList" } &&
+                   toListSymbol.ReturnType is INamedTypeSymbol toListReturnType &&
+                   toListReturnType.OriginalDefinition.ToDisplayString() == "System.Collections.Generic.List<T>" &&
+                   forEachSymbol is { Name: "ForEach" } &&
+                   SymbolEqualityComparer.Default.Equals(forEachSymbol.ContainingType, toListReturnType);
         }
     }
 }
