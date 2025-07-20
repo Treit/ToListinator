a8af375680773fc5bb2cc6aa0996c1d53f519977
Mike Treit <mtreit@ntdev.microsoft.com>
2025-07-20 16:56:50 -0700

    Update workflow permissions

--- test/ToListinator.TestApp/Program.cs
+++ test/ToListinator.TestApp/Program.cs
@@ -9,25 +9,25 @@ namespace ToListinator.TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Testing ToListinator analyzer...");
-            
+
             var numbers = new[] { 1, 2, 3, 4, 5, 6 };
-            
+
             // This should trigger the analyzer warning
             numbers.ToList().ForEach(x => Console.WriteLine($"Number: {x}"));
-            
+
             // This should also trigger the analyzer warning
             numbers.Where(x => x > 2).ToList().ForEach(x => Console.WriteLine($"Filtered: {x}"));
-            
+
             // This should NOT trigger the analyzer warning (regular foreach)
             foreach (var number in numbers)
             {
                 Console.WriteLine($"Regular foreach: {number}");
             }
-            
+
             // This should NOT trigger the analyzer warning (ToList without ForEach)
             var list = numbers.ToList();
             Console.WriteLine($"List count: {list.Count}");
-            
+
             Console.WriteLine("Done!");
         }
     }
