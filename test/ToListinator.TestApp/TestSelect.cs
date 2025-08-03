9906c27d57a13bb769aee5b5c05ed39df0e76aba
Mike Treit <mtreit@hotmail.com>
2025-08-03 12:21:20 -0700

    Add example code for null coalesce / loop case

--- test/ToListinator.TestApp/TestSelect.cs
+++ test/ToListinator.TestApp/TestSelect.cs
@@ -10,6 +10,14 @@ public class TestCode
         numbers.Where(x => x > 1).ToList().ForEach(Console.Write);
 
         var selected = numbers.Select(x => x).Where(x => x > 1);
+
+        // Print the numbers!
+        foreach (var num in numbers ?? Array.Empty<int>()) // Will only run if numbers is not null
+        {
+            // Some comment about the loop
+            Print(num); // Output!
+        }
+
     }
 
     void Print(int x)
